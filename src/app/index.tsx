ab4f90fbaada9af06e471be304f770b9aba39980
Martin Hoff Teigen <martin@teigen.dev>
2026-07-02 17:39:47 +0100

    2 Create layout for simple connection app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,35 @@
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 
 export default function Index() {
     return (
-        <View>
-            <Text>My app</Text>
+        <View className="gap-4 p-2">
+            <View className="gap-2">
+                <Text className="text-xl">Glasses Bluetooth</Text>
+                <Text>Status: Not connected</Text>
+                <View className="gap-2">
+                    <Button title="Scan for glasses" />
+                    <Button title="Connect to glasses" />
+                </View>
+            </View>
+
+            <View className="gap-2">
+                <Text className="text-xl">Glasses WiFi</Text>
+                <Text>Status: Not connected</Text>
+                <Text>SSID: ....</Text>
+                <Text>IP: ....</Text>
+                <View className="gap-2">
+                    <Button title="Refresh status" />
+                    <Button title="Connect to wifi" />
+                </View>
+            </View>
+
+            <View className="gap-2">
+                <Text>Devices connected to same WiFi</Text>
+                {/*<ul>*/}
+                {/*    <li>Phone: Eduroam</li>*/}
+                {/*    <li>Glasses: Eduroam</li>*/}
+                {/*</ul>*/}
+            </View>
         </View>
     )
 }
