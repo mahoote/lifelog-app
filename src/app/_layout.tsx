3b27ba4403ef85e99cc58d7130e99535d6ecbf6b
Martin Hoff Teigen <martin@teigen.dev>
2026-07-02 18:52:58 +0100

    2 Add title to the index page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,7 +6,9 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler'
 export default function RootLayout() {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <Stack />
+            <Stack>
+                <Stack.Screen name="index" options={{ title: 'Lifelog' }} />
+            </Stack>
         </GestureHandlerRootView>
     )
 }
