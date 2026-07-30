df3ea396536a67f1fe177869a3ae9cc6b5b26d12
Martin Hoff Teigen <martin@teigen.dev>
2026-07-31 00:47:51 +0100

    7 Skips existing downloads

--- src/utils/downloadUtils.ts
+++ src/utils/downloadUtils.ts
@@ -1,5 +1,6 @@
 import { ActionCreatorWithPayload } from '@reduxjs/toolkit'
 import { saveCaptureEvent } from '@/repositories/captureEventRepository'
+import { getFootageItemById } from '@/repositories/footageItemRepository'
 import { downloadFootageById } from '@/services/lifelogService'
 import { AppDispatch } from '@/store/hooks'
 import { CaptureEvent } from '@/types/captureEvent'
@@ -68,6 +69,17 @@ export async function downloadCaptureEventFootage(
 
     for (const footageItem of captureEvent.footageItems) {
         if (!footageItem.id) {
+            continue
+        }
+
+        const existingFootageItem = await getFootageItemById(footageItem.id)
+
+        if (existingFootageItem?.fileUri) {
+            results.push({
+                id: footageItem.id,
+                uri: existingFootageItem.fileUri,
+                continue: true,
+            })
             continue
         }
 
